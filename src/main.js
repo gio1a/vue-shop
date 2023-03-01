3ca4128dfa11eda75b0e3a1289e2170f43b4b7f5
gio1a <zengv@foxmail.com>
2023-03-01 14:07:02 +0800

    完成导航菜单设置及菜单的路由跳转

--- src/main.js
+++ src/main.js
@@ -11,6 +11,12 @@ Vue.config.productionTip = false
 
 // 配置请求根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 通过axios请求拦截器，在请求头中添加Authorization字段
+axios.interceptors.request.use( config => {
+    console.log('axios 拦截下的请求 -----> ',config)
+    config.headers.Authorization = window.sessionStorage.getItem('token')
+    return config
+})
 Vue.prototype.$http = axios
 
 new Vue({
